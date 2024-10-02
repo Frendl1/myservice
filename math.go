fa3d88c513158e5a8a9fa4957efec2682b0e5646
Frendl1 <128584784+Frendl1@users.noreply.github.com>
2024-10-02 11:50:36 +0300

    First commit

---
+++ math.go
@@ -0,0 +1,11 @@
+package mathutils
+
+// Add возвращает сумму двух целых чисел
+func Add(a, b int) int {
+    return a + b
+}
+
+// Subtract возвращает разность двух целых чисел
+func Sub(a, b int) int {
+    return a - b
+}
